f10b89a5682469fb6e1354bed3ac01ca932c7def
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2024-03-22 15:30:51 -0300

    feat: home section and animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
-/* Script */
+function animation(targetSelector, showClass, hideClass) {
+  const element = document.querySelector(targetSelector);
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add(showClass);
+        entry.target.classList.remove(hideClass);
+      } else {
+        entry.target.classList.remove(showClass);
+        entry.target.classList.add(hideClass);
+      }
+    });
+  });
+
+  observer.observe(element);
+}
+
+animation('.intro', 'intro--animation-show', 'intro--animation-hidden');
