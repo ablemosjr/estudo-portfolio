4e682b8008e96b14cecad028ae9cd7616c646036
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2024-03-22 21:22:25 -0300

    feat: project section and card animation

--- src/js/animations.js
+++ src/js/animations.js
@@ -16,6 +16,23 @@ function animation(targetSelector, showClass, hideClass) {
   observer.observe(element);
 }
 
+// Card Animation
+const card = document.querySelectorAll('.card');
+const hoverReveal = document.querySelectorAll('.card__image');
+
+for (let index = 0; index < card.length; index++) {
+  card[index].addEventListener('mousemove', (e) => {
+    hoverReveal[index].style.opacity = 1;
+    hoverReveal[index].style.transform = 'translate(-100%, -50%) rotate(5deg)';
+    hoverReveal[index].style.left = e.clientX + 'px';
+  });
+
+  card[index].addEventListener('mouseleave', (e) => {
+    hoverReveal[index].style.opacity = 0;
+    hoverReveal[index].style.transform = 'translate(-200%, -50%) rotate(-5deg)';
+  });
+}
+
 // Home Animation
 animation('.intro', 'intro--animation-show', 'intro--animation-hidden');
 
